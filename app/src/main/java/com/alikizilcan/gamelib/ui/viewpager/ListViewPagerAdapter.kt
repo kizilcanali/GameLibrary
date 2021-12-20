770f0f2e883a9791f27f468f9b5fa715ce5603e1
Ali Kızılcan <kizilcanali@gmail.com>
2021-12-20 16:11:44 +0300

    viewpager optimization(layouts), viewpager implementation

---
+++ app/src/main/java/com/alikizilcan/gamelib/ui/viewpager/ListViewPagerAdapter.kt
@@ -0,0 +1,44 @@
+package com.alikizilcan.gamelib.ui.viewpager
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.alikizilcan.gamelib.databinding.PagerItemBinding
+import com.alikizilcan.gamelib.domain.model.Game
+
+class ListViewPagerAdapter() : ListAdapter<Game, ListViewPagerAdapter.GameViewHolder>(DIFF_CALLBACK){
+
+    var itemClickListener: (Game) -> Unit = {}
+
+    class GameViewHolder(private val binding: PagerItemBinding, private val itemClickListener: (Game) -> Unit) : RecyclerView.ViewHolder(binding.root){
+        fun bind(game : Game){
+            binding.baseModel = game
+            binding.executePendingBindings()
+
+            binding.root.setOnClickListener {
+                itemClickListener(game)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GameViewHolder {
+        val binding = PagerItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return GameViewHolder(binding, itemClickListener)
+    }
+
+    override fun onBindViewHolder(holder: GameViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object{
+        val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Game>() {
+            override fun areItemsTheSame(oldItem: Game, newItem: Game): Boolean = oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Game, newItem: Game): Boolean = oldItem == newItem
+
+        }
+    }
+
+}
